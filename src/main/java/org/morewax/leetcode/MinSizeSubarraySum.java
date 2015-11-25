190a4141dfbd5186c79a5841f1460c8e6d1b3fc4
yuanbing <yuanbing@gmail.com>
2015-11-24 22:00:05 -0800

    O(nlogn) solution for minimum size subarray sum problem

--- src/main/java/org/morewax/leetcode/MinSizeSubarraySum.java
+++ src/main/java/org/morewax/leetcode/MinSizeSubarraySum.java
@@ -12,6 +12,8 @@ package org.morewax.leetcode;
  * Created by byuan on 11/24/15.
  */
 public class MinSizeSubarraySum {
+    /**
+     * O(N) solution
     public int minSubArrayLen(int s, int[] nums) {
         if (nums.length == 0) return 0;
 
@@ -33,6 +35,45 @@ public class MinSizeSubarraySum {
         }
 
         return (minLength == n+1)? 0 : minLength;
+    }*/
+
+    // O(nlogn) solution
+    public int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length;
+        int[] sums = new int[n+1];
+        int minLength = n+1;
+
+        for (int i = 0; i < n; ++i) {
+            sums[i+1] = sums[i] + nums[i];
+
+            if (sums[i+1] >= s) {
+                int j = binarySearch(sums, 0, i, sums[i+1]-s);
+                if (j != -1) {
+                    minLength = Math.min(minLength, i+1-j);
+                }
+            }
+        }
+
+        return (minLength == n+1)? 0 : minLength;
+    }
+
+    // Search for the largest index i so that
+    // A[i] <= target and start <= i && i <= end
+    private int binarySearch(int[] A, int start, int end, int target) {
+        int l = start-1;
+        int h = end+1;
+
+        while (l+1 < h) {
+            int m = l + (h-l)/2;
+
+            if (A[m] > target) {
+                h = m;
+            } else {
+                l = m;
+            }
+        }
+
+        return (l < start)? -1 : l;
     }
 
     public static void main(String[] args) {
@@ -42,8 +83,12 @@ public class MinSizeSubarraySum {
 
         int[] a1 = new int[]{2,3,1,2,4,3};
         s = 7;
-
         result = solution.minSubArrayLen(s, a1);
+        System.out.println("Minimum size: " + result);
+
+        int[] a2 = new int[]{1, 4, 4};
+        s = 4;
+        result = solution.minSubArrayLen(s, a2);
         System.out.println("Minimum size: " + result);
     }
 }

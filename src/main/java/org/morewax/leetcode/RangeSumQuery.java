87f4e5e024d41cd437d50ee1b19a7f42830148fa
yuanbing <yuanbing@gmail.com>
2015-11-25 23:11:02 -0800

    Range Sum Query (Mutable) Segment Tree Impl

--- src/main/java/org/morewax/leetcode/RangeSumQuery.java
+++ src/main/java/org/morewax/leetcode/RangeSumQuery.java
@@ -24,6 +24,8 @@ package org.morewax.leetcode;
  *  2. Use Segment Tree
  */
 public class RangeSumQuery {
+    /**
+     * Binary Index Tree
     private int[] num;
     private int[] bit;
 
@@ -56,6 +58,76 @@ public class RangeSumQuery {
         }
 
         return res;
+    }*/
+
+    // Segment Tree Impl
+    private int[] segmentTree;
+    private int[] data; // for the original data
+
+    public RangeSumQuery(int[] nums) {
+        // make a copy of nums
+        int n = nums.length;
+        data = new int[n];
+
+        System.arraycopy(nums, 0, data, 0, n);
+
+        int segmentTreeHeight = (int)Math.ceil(Math.log(n)/Math.log(2));
+        int segmentTreeSize = 2*(int)Math.pow(2, segmentTreeHeight) - 1;
+        segmentTree = new int[segmentTreeSize];
+
+        buildSegmentTree(0, n-1, 0);
+    }
+
+    public int sumRange(int i, int j) {
+        return sumRangeHelper(0, data.length-1, i, j, 0);
+    }
+
+    public void update(int i, int val) {
+        int diff = val - data[i];
+
+        data[i] = val;
+
+        updateHelper(0, data.length-1, i, diff, 0);
+    }
+
+    private int buildSegmentTree(int start, int end, int segIndex) {
+        if (start == end) {
+            segmentTree[segIndex] = data[start];
+            return segmentTree[segIndex];
+        }
+
+        int m = getMiddle(start, end);
+        segmentTree[segIndex] = buildSegmentTree(start, m, 2*segIndex+1) + buildSegmentTree(m+1, end, 2*segIndex+2);
+        return segmentTree[segIndex];
+    }
+
+    private int sumRangeHelper(int ss, int se, int qs, int qe, int si) {
+        if (qs <= ss && se <= qe) {
+            return segmentTree[si];
+        }
+
+        if (se < qs || qe < ss) return 0;
+
+        int mid = getMiddle(ss, se);
+
+        return sumRangeHelper(ss, mid, qs, qe, 2*si+1) + sumRangeHelper(mid+1, se, qs, qe, 2*si+2);
+    }
+
+    private void updateHelper(int ss, int se, int i, int diff, int si) {
+        if (i < ss || se < i) return;
+
+        segmentTree[si] += diff;
+
+        if (ss != se) {
+            int mid = getMiddle(ss, se);
+
+            updateHelper(ss, mid, i, diff, 2*si+1);
+            updateHelper(mid+1, se, i, diff, 2*si+2);
+        }
+    }
+
+    private int getMiddle(int start, int end) {
+        return start + (end-start)/2;
     }
 
     public static void main(String[] args) {

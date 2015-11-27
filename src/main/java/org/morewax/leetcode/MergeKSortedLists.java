1a7d96384fa4175dd0b7517172501b26aa279280
yuanbing <yuanbing@gmail.com>
2015-11-27 11:27:58 -0800

    1. Merge k Sorted Lists; 2. Palindrome List

---
+++ src/main/java/org/morewax/leetcode/MergeKSortedLists.java
@@ -0,0 +1,70 @@
+package org.morewax.leetcode;
+
+import org.morewax.leetcode.support.ListNode;
+
+/**
+ * Created by Bing on 11/27/2015.
+ */
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+
+        return helper(lists, 0, lists.length-1);
+    }
+
+    private ListNode helper(ListNode[] lists, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        if (start == end) {
+            return lists[start];
+        }
+
+        int mid = start + (end-start)/2;
+
+        ListNode left = helper(lists, start, mid);
+        ListNode right = helper(lists, mid+1, end);
+
+        return mergeTwoLists(left, right);
+    }
+
+    private ListNode mergeTwoLists(ListNode left, ListNode right) {
+        if (left != null && right == null) return left;
+        if (right != null && left == null) return right;
+
+        ListNode[] newList = new ListNode[2];
+
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                ListNode next = left.next;
+                append(newList, left);
+                left = next;
+            } else {
+                ListNode next = right.next;
+                append(newList, right);
+                right = next;
+            }
+        }
+
+        if (left != null) {
+            append(newList, left);
+        }
+
+        if (right != null) {
+            append(newList, right);
+        }
+
+        return newList[0];
+    }
+
+    private void append(ListNode[] newList, ListNode node) {
+        if (newList[1] == null) {
+            newList[0] = node;
+        } else {
+            newList[1].next = node;
+        }
+
+        newList[1] = node;
+    }
+}
---
+++ src/main/java/org/morewax/leetcode/PalindromeList.java
@@ -0,0 +1,68 @@
+package org.morewax.leetcode;
+
+import org.morewax.leetcode.support.ListNode;
+
+/**
+ * Created by Bing on 11/27/2015.
+ */
+public class PalindromeList {
+    public boolean isPalindrome(ListNode head) {
+        int n = getLength(head);
+
+        if (n <= 1) return true;
+
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode secondHalf;
+        int steps;
+
+        if (n%2 == 0) { // even number of nodes
+            steps = n/2;
+        } else {
+            steps = n/2 + 1;
+        }
+
+        for (int step = 1; step <= steps; ++step) {
+            prev = current;
+            current = current.next;
+        }
+
+        secondHalf = reverse(current);
+
+        prev.next = secondHalf;
+        current = head;
+
+        while (current != null && secondHalf != null) {
+            if (current.val != secondHalf.val) return false;
+            current = current.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private int getLength(ListNode head) {
+        int count = 0;
+
+        while (head != null) {
+            ++count;
+            head = head.next;
+        }
+
+        return count;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}

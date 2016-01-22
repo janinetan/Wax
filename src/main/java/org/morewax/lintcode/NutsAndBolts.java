04c10c5c08a99e38b7fb1ca86ad80f17b16fa22e
byuan <bing_yuan@intuit.com>
2016-01-22 13:35:19 -0800

    Lintcode: 1. Nuts and Bolts

--- src/main/java/org/morewax/lintcode/NutsAndBolts.java
+++ src/main/java/org/morewax/lintcode/NutsAndBolts.java
@@ -5,8 +5,22 @@ import org.morewax.support.Utils;
 
 /**
  * Problem
- * Nuts and Bolts ()
- * https://s3.amazonaws.com/uploads.hipchat.com/87219/1990749/WoNJaBcQJkIMfMd/upload.png
+ * Nuts and Bolts (http://www.lintcode.com/en/problem/nuts-bolts-problem/)
+ * Given a set of n nuts of different sizes and n bolts of different sizes. There is a one-one mapping between nuts and
+ * bolts. Comparison of a nut to another nut or a bolt to another bolt is not allowed. It means nut can only be compared
+ * with bolt and bolt can only be compared with nut to see which one is bigger/smaller.
+ * We will give you a compare function to compare nut with bolt.
+ *  Example
+ *      Given nuts = ['ab','bc','dd','gg'], bolts = ['AB','GG', 'DD', 'BC'].
+ * Your code should find the matching bolts and nuts.
+ * one of the possible return:
+ * nuts = ['ab','bc','dd','gg'], bolts = ['AB','BC','DD','GG'].
+ * we will tell you the match compare function. If we give you another compare function.
+ * the possible return is the following:
+ * nuts = ['ab','bc','dd','gg'], bolts = ['BC','AA','DD','GG'].
+ * So you must use the compare function that we give to do the sorting.
+ * The order of the nuts or bolts does not matter. You just need to find the matching bolt for each nut.
+ *
  * Created by byuan on 1/21/16.
  */
 public class NutsAndBolts {
@@ -33,24 +47,21 @@ public class NutsAndBolts {
 
     private int split(String[] collection, int l, int h, String pivot, NBComparator compare) {
         int smaller = l-1;
-        int equal = l-1;
 
-        for (int i = l; i <= h; ++i) {
-            int result = compare.cmp(collection[i], pivot);
-            if (result == 1) {
-                continue;
-            } else if (result == 0) {
-                swap(collection, equal+1, i);
-                ++equal;
-            } else { // result == -1
-                swap(collection, equal+1, i);
-                swap(collection, equal+1, smaller+1);
+        for (int i = l; i < h; ++i) {
+            if (compare.cmp(collection[i], pivot) == -1 || compare.cmp(pivot, collection[i]) == -1) {
                 ++smaller;
-                ++equal;
+                swap(collection, i, smaller);
+            } else if (compare.cmp(collection[i], pivot) == 0 || compare.cmp(pivot, collection[i]) == 0) {
+                swap(collection, i, h);
+                --i;
             }
         }
 
-        return smaller+1;
+        ++smaller;
+        swap(collection, smaller, h);
+
+        return smaller;
     }
 
     private void swap(String[] collection, int i, int j) {

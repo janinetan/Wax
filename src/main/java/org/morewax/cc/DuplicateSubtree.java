969c7dca41913209dfb48cfd07e18eea5730dbbc
yuanbing <yuanbing@gmail.com>
2016-02-18 23:29:57 -0800

    CC:
    1. Skeleton for detecting duplicated subtrees

---
+++ src/main/java/org/morewax/cc/DuplicateSubtree.java
@@ -0,0 +1,10 @@
+package org.morewax.cc;
+
+/**
+ * Problem
+ * Find if a given binary tree has duplicate sub trees or not.
+ * (Two leaf nodes of a parent do not count as subtree)
+ * Created by Bing on 2/18/2016.
+ */
+public class DuplicateSubtree {
+}
